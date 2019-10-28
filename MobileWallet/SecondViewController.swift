4a2d329d7cee10ea03ee506b5ad42e9afc4a858b
Jason <jayvdb1@gmail.com>
2019-10-28 10:57:03 +0200

    Initial project setup

---
+++ MobileWallet/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MobileWallet
+//
+//  Created by Jason van den Berg on 2019/10/28.
+//  Copyright © 2019 Jason van den Berg. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
