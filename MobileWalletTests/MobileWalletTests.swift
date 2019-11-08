f13011ab3f26c27f66a3ff403144dd11eb509506
Gugulethu <mhlangagc@users.noreply.github.com>
2019-11-08 20:55:42 +0200

    word change verification test

--- MobileWalletTests/MobileWalletTests.swift
+++ MobileWalletTests/MobileWalletTests.swift
@@ -138,7 +138,7 @@ class MobileWalletTests: XCTestCase {
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            // Add the code you want to measure the time of here.
         }
     }
 
