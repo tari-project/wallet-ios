5bd30be66b4266df9dddf6a4f970bf4aff79cae1
gabrielLupu <lupugabriel@gmail.com>
2020-03-03 11:18:39 +0000

    feat: Removing Profile Storyboard.

--- MobileWallet/Screens/Home/HomeViewController.swift
+++ MobileWallet/Screens/Home/HomeViewController.swift
@@ -340,10 +340,8 @@ class HomeViewController: UIViewController, FloatingPanelControllerDelegate, Tra
     }
 
     @IBAction func onProfileAction(_ sender: Any) {
-        let storyboard = UIStoryboard.init(name: "Profile", bundle: nil)
-        if let vc = storyboard.instantiateViewController(identifier: "ProfileViewController") as? ProfileViewController {
-            self.present(vc, animated: true, completion: nil)
-        }
+        let vc = ProfileViewController()
+        self.present(vc, animated: true, completion: nil)
     }
     // MARK: - TransactionTableDelegateMethods
 
