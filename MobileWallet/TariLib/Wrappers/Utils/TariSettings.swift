eede0c9afd6ae3a9cc3232be0d2cb355303ad133
Jasonvdb <jayvdb1@gmail.com>
2020-04-03 15:29:34 +0200

    Picking a basenode randomly from a pool.

--- MobileWallet/TariLib/Wrappers/Utils/TariSettings.swift
+++ MobileWallet/TariLib/Wrappers/Utils/TariSettings.swift
@@ -82,18 +82,35 @@ struct TariSettings {
     let torEnabled = true
     #endif
 
-    let defaultBaseNodePeer = "2e93c460df49d8cfbbf7a06dd9004c25a84f92584f7d0ac5e30bd8e0beee9a43::/onion3/nuuq3e2olck22rudimovhmrdwkmjncxvwdgbvfxhz6myzcnx2j4rssyd:18141"
+    let defaultBaseNodePool: [String] = [
+        //Faucet
+        "2e93c460df49d8cfbbf7a06dd9004c25a84f92584f7d0ac5e30bd8e0beee9a43::/onion3/nuuq3e2olck22rudimovhmrdwkmjncxvwdgbvfxhz6myzcnx2j4rssyd:18141",
+        //#t-tbn-nvir
+        "06e98e9c5eb52bd504836edec1878eccf12eb9f26a5fe5ec0e279423156e657a::/onion3/bsmuof2cn4y2ysz253gzsvg3s72fcgh4f3qcm3hdlxdtcwe6al2dicyd:18141",
+        //#t-tbn-ncal
+        "3a5081a0c9ff72b2d5cf52f8d78cc5a206d643259cdeb7d934512f519e090e6c::/onion3/gfynjxfm7rcxcekhu6jwuhvyyfdjvmruvvehurmlte565v74oinv2lad:18141",
+        //#t-tbn-oregon
+        "e6f3c83dc592af45ede5424974f52c776e9e6859e530066e57c1b0dd59d9b61c::/onion3/ixihtmcbvr2q53bj23bsla5hi7ek37odnsxzkapr7znyfneqxzhq7zad:18141",
+        //#t-tbn-london
+        "ce2254825d0e0294d31a86c6aac18f83c9a7b3d01d9cdb6866b4b2af8fd3fd17::/onion3/gm7kxmr4cyjg5fhcw4onav2ofa3flscrocfxiiohdcys3kshdhcjeuyd:18141",
+        //#t-tbn-stockholm
+        "461d4d7be657521969896f90e3f611f0c4e902ca33d3b808c03357ad86fd7801::/onion3/4me2aw6auq2ucql34uuvsegtjxmvsmcyk55qprtrpqxvu3whxajvb5ad:18141",
+        //#t-tbn-seoul
+        "d440b328e69b20dd8ee6c4a61aeb18888939f0f67cf96668840b7f72055d834c::/onion3/j5x7xkcxnrich5lcwibwszd5kylclbf6a5unert5sy6ykio2kphnopad:18141",
+        //#t-tbn-sydney
+        "b81b4071f72418cc410166d9baf0c6ef7a8c309e64671fafbbed88f7e1ee7709::/onion3/lwwcv4nq7epgem5vdcawom4mquqsw2odbwfcjzv3j6sksx4gr24e52ad:18141"
+    ]
+
+    func getRandomBaseNode() -> String {
+        return defaultBaseNodePool[Int.random(in: 0 ... (defaultBaseNodePool.count-1))]
+    }
 
     #if DEBUG
     //Used for showing a little extra detail in the UI to help debugging
     let isDebug = true
-    //Local macbook node
-    //let defaultBaseNodePeer = "626b71ffe979bda1cb9b8c411c68435de0a966bd50ae324d67e31bd6710c8f58::/onion3/5usjvbf5rprsgdn5bxwelj5mzlcjfrpwhhklkwtcq2cl4ehmexiolvyd:18141"
     let maxMbLogsStorage: UInt64 = 5000 //5GB
     #else
     let isDebug = false
-    //Taribot faucet node
-//    let defaultBaseNodePeer = "2e93c460df49d8cfbbf7a06dd9004c25a84f92584f7d0ac5e30bd8e0beee9a43::/onion3/nuuq3e2olck22rudimovhmrdwkmjncxvwdgbvfxhz6myzcnx2j4rssyd:18141"
     let maxMbLogsStorage: UInt64 = 500 //500MB
     #endif
 }
