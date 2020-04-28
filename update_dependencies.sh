4c5df325121e42c3dc6005ce685a2256eed7c634
crypt0ginge <7778242+crypt0ginge@users.noreply.github.com>
2020-04-28 15:31:37 +0200

    Update to check for and install pods and carthage.

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -3,25 +3,33 @@
 echo "\n\n***Pulling latest Tari lib build***"
 source dependencies.env
 if [[ -z "$FFI_VERSION" ]]; then
-	FFI_FILE="${1:-$(curl -s --compressed "https://www.tari.com/downloads/" | egrep -o  'libtari_wallet_ffi-ios-[0-9\.]+.tar.gz' | sort -V  | tail -1)}"
+        FFI_FILE="${1:-$(curl -s --compressed "https://www.tari.com/downloads/" | egrep -o  'libtari_wallet_ffi-ios-[0-9\.]+.tar.gz' | sort -V  | tail -1)}"
 else
-	FFI_FILE="libtari_wallet_ffi-ios-$FFI_VERSION.tar.gz"
+        FFI_FILE="libtari_wallet_ffi-ios-$FFI_VERSION.tar.gz"
 fi
 
 curl -s "https://www.tari.com/binaries/$FFI_FILE" | tar xz - -C MobileWallet/TariLib/ --exclude wallet.h
 
-# Check for cocoapods and install if missing. Install carthage.
+# Check for cocoapods and install if missing.
 if hash pod 2>/dev/null; then
   echo "Cool, you have pods installed."
 else
   echo "You need cocoapods. Would you like it installed?"
-  read -e -p "Y or N? " yn
+  read -e -p "y or n? " yn
   if [[ "y" = "$yn" || "Y" = "$yn" ]]; then
     sudo gem install cocoapods
+  fi
+fi
+# Check for carthage and install if missing.
+if hash carthage 2>/dev/null; then
+  echo "Cool, you have carthage installed."
+else
+  echo "You need carthage. Would you like it installed?"
+  read -e -p "y or n? " yn
+  if [[ "y" = "$yn" || "Y" = "$yn" ]]; then
     brew install carthage
   fi
 fi
-
 echo "\n\n***Updating pods***"
 pod install
 
