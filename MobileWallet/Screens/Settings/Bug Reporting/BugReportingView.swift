97ad0cf055a6b64a1d19b0db1232ddf0297a8a40
Adrian Truszczyński <adrian.truszczynski@gmail.com>
2022-11-18 10:32:26 +0100

    Bug Reporting Screen UI Issues
    - Added scrollable content view to be able fit the whole content on the screen even on small devices.
    - Changed return type from "default/return" to "Done"
    - The app with now hide keyboard when user tap on "Done" button.

--- MobileWallet/Screens/Settings/Bug Reporting/BugReportingView.swift
+++ MobileWallet/Screens/Settings/Bug Reporting/BugReportingView.swift
@@ -45,11 +45,14 @@ final class BugReportingView: BaseNavigationContentView {
     
     // MARK: - Subviews
     
+    @View private var mainContentView = KeyboardAvoidingContentView()
+    
     @View private var headerLabel: UILabel = {
         let view = UILabel()
         view.text = localized("bug_reporting.label.header")
         view.textColor = .tari.text.heading
         view.font = .Avenir.medium.withSize(13.0)
+        view.numberOfLines = 0
         return view
     }()
     
@@ -58,6 +61,7 @@ final class BugReportingView: BaseNavigationContentView {
         view.placeholder = localized("bug_reporting.text_field.name")
         view.font = .Avenir.medium.withSize(14.0)
         view.textColor = .tari.text.heading
+        view.returnKeyType = .done
         return view
     }()
     
@@ -72,6 +76,7 @@ final class BugReportingView: BaseNavigationContentView {
         view.placeholder = localized("bug_reporting.text_field.email")
         view.font = .Avenir.medium.withSize(14.0)
         view.textColor = .tari.text.heading
+        view.returnKeyType = .done
         return view
     }()
     
@@ -86,6 +91,7 @@ final class BugReportingView: BaseNavigationContentView {
         view.text = localized("bug_reporting.label.message_header")
         view.textColor = .tari.text.heading
         view.font = .Avenir.medium.withSize(13.0)
+        view.numberOfLines = 0
         return view
     }()
     
@@ -96,6 +102,7 @@ final class BugReportingView: BaseNavigationContentView {
         view.textColor = .tari.text.heading
         view.font = .Avenir.medium.withSize(13.0)
         view.textContainerInset = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+        view.returnKeyType = .done
         return view
     }()
     
@@ -156,43 +163,48 @@ final class BugReportingView: BaseNavigationContentView {
     
     private func setupConstraints() {
         
-        [headerLabel, nameTextField, nameTextFieldSeparator, emailTextField, emailTextFieldSeparator, messageHeaderLabel, messageTextView, footerLabel, sendButton, logsButton].forEach { addSubview($0) }
+        addSubview(mainContentView)
+        [headerLabel, nameTextField, nameTextFieldSeparator, emailTextField, emailTextFieldSeparator, messageHeaderLabel, messageTextView, footerLabel, sendButton, logsButton].forEach { mainContentView.contentView.addSubview($0) }
         
         let constraints = [
-            headerLabel.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 34.0),
-            headerLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25.0),
-            headerLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25.0),
+            mainContentView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor),
+            mainContentView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mainContentView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mainContentView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            headerLabel.topAnchor.constraint(equalTo: mainContentView.contentView.topAnchor, constant: 34.0),
+            headerLabel.leadingAnchor.constraint(equalTo: mainContentView.contentView.leadingAnchor, constant: 25.0),
+            headerLabel.trailingAnchor.constraint(equalTo: mainContentView.contentView.trailingAnchor, constant: -25.0),
             nameTextField.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 26.0),
-            nameTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25.0),
-            nameTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25.0),
+            nameTextField.leadingAnchor.constraint(equalTo: mainContentView.contentView.leadingAnchor, constant: 25.0),
+            nameTextField.trailingAnchor.constraint(equalTo: mainContentView.contentView.trailingAnchor, constant: -25.0),
             nameTextFieldSeparator.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 26.0),
-            nameTextFieldSeparator.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25.0),
-            nameTextFieldSeparator.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25.0),
+            nameTextFieldSeparator.leadingAnchor.constraint(equalTo: mainContentView.contentView.leadingAnchor, constant: 25.0),
+            nameTextFieldSeparator.trailingAnchor.constraint(equalTo: mainContentView.contentView.trailingAnchor, constant: -25.0),
             nameTextFieldSeparator.heightAnchor.constraint(equalToConstant: 1.0),
             emailTextField.topAnchor.constraint(equalTo: nameTextFieldSeparator.bottomAnchor, constant: 26.0),
-            emailTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25.0),
-            emailTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25.0),
+            emailTextField.leadingAnchor.constraint(equalTo: mainContentView.contentView.leadingAnchor, constant: 25.0),
+            emailTextField.trailingAnchor.constraint(equalTo: mainContentView.contentView.trailingAnchor, constant: -25.0),
             emailTextFieldSeparator.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 26.0),
-            emailTextFieldSeparator.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25.0),
-            emailTextFieldSeparator.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25.0),
+            emailTextFieldSeparator.leadingAnchor.constraint(equalTo: mainContentView.contentView.leadingAnchor, constant: 25.0),
+            emailTextFieldSeparator.trailingAnchor.constraint(equalTo: mainContentView.contentView.trailingAnchor, constant: -25.0),
             emailTextFieldSeparator.heightAnchor.constraint(equalToConstant: 1.0),
             messageHeaderLabel.topAnchor.constraint(equalTo: emailTextFieldSeparator.bottomAnchor, constant: 20.0),
-            messageHeaderLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25.0),
-            messageHeaderLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25.0),
+            messageHeaderLabel.leadingAnchor.constraint(equalTo: mainContentView.contentView.leadingAnchor, constant: 25.0),
+            messageHeaderLabel.trailingAnchor.constraint(equalTo: mainContentView.contentView.trailingAnchor, constant: -25.0),
             messageTextView.topAnchor.constraint(equalTo: messageHeaderLabel.bottomAnchor, constant: 10.0),
-            messageTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25.0),
-            messageTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25.0),
+            messageTextView.leadingAnchor.constraint(equalTo: mainContentView.contentView.leadingAnchor, constant: 25.0),
+            messageTextView.trailingAnchor.constraint(equalTo: mainContentView.contentView.trailingAnchor, constant: -25.0),
             messageTextView.heightAnchor.constraint(greaterThanOrEqualToConstant: 200.0), // TODO: !
             footerLabel.topAnchor.constraint(equalTo: messageTextView.bottomAnchor, constant: 20.0),
-            footerLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25.0),
-            footerLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25.0),
+            footerLabel.leadingAnchor.constraint(equalTo: mainContentView.contentView.leadingAnchor, constant: 25.0),
+            footerLabel.trailingAnchor.constraint(equalTo: mainContentView.contentView.trailingAnchor, constant: -25.0),
             sendButton.topAnchor.constraint(equalTo: footerLabel.bottomAnchor, constant: 20.0),
-            sendButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25.0),
-            sendButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25.0),
+            sendButton.leadingAnchor.constraint(equalTo: mainContentView.contentView.leadingAnchor, constant: 25.0),
+            sendButton.trailingAnchor.constraint(equalTo: mainContentView.contentView.trailingAnchor, constant: -25.0),
             logsButton.topAnchor.constraint(equalTo: sendButton.bottomAnchor, constant: 20.0),
-            logsButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25.0),
-            logsButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25.0),
-            logsButton.bottomAnchor.constraint(equalTo: safeBottomAnchor, constant: -30.0)
+            logsButton.leadingAnchor.constraint(equalTo: mainContentView.contentView.leadingAnchor, constant: 25.0),
+            logsButton.trailingAnchor.constraint(equalTo: mainContentView.contentView.trailingAnchor, constant: -25.0),
+            logsButton.bottomAnchor.constraint(equalTo: mainContentView.contentView.bottomAnchor, constant: -30.0)
         ]
         
         NSLayoutConstraint.activate(constraints)
@@ -207,11 +219,33 @@ final class BugReportingView: BaseNavigationContentView {
         logsButton.onTap = { [weak self] in
             self?.onShowLogsButtonTap?()
         }
+        
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+        messageTextView.delegate = self
     }
     
     // MARK: - Updates
     
     private func updateSendButton() {
         sendButton.variation = isProcessing ? .loading : .normal
+    }
+}
+
+extension BugReportingView: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension BugReportingView: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        guard text == "\n" else { return true }
+        textView.resignFirstResponder()
+        return false
+        
     }
 }
