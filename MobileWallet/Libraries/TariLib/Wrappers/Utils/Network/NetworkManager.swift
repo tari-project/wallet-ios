8c68669ed63e40cab45a17cff618d6c58de577fa
Adrian Truszczyński <TruszczynskiA@users.noreply.github.com>
2024-04-24 15:00:29 +0200

    Peer DB Migration (#1057)
    - Added a one-time peer db migration feature
    - Updated FFI Library to v1.0.0-rc.8
    - Switched used network to NextNet

--- MobileWallet/Libraries/TariLib/Wrappers/Utils/Network/NetworkManager.swift
+++ MobileWallet/Libraries/TariLib/Wrappers/Utils/Network/NetworkManager.swift
@@ -49,7 +49,7 @@ final class NetworkManager {
     // MARK: - Properties
 
     static let shared = NetworkManager()
-    private static var defaultNetwork: TariNetwork { .stagenet }
+    private static var defaultNetwork: TariNetwork { .nextnet }
 
     @Published var selectedNetwork: TariNetwork
 
