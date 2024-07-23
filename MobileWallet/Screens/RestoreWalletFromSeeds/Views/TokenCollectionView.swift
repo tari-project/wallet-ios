e0040ee6584aec359723b09532c57d1eb7559f19
Adrian Truszczyński <TruszczynskiA@users.noreply.github.com>
2024-07-23 15:10:09 +0200

    Layout issue on the seed words verification screen (#1092)
    - Fixed reported issue. Now, UI elements on the seed words verification screen will layout properly.

--- MobileWallet/Screens/RestoreWalletFromSeeds/Views/TokenCollectionView.swift
+++ MobileWallet/Screens/RestoreWalletFromSeeds/Views/TokenCollectionView.swift
@@ -214,7 +214,7 @@ final class TokenCollectionView: DynamicThemeView {
         var snapshot = NSDiffableDataSourceSnapshot<Int, SeedWordModel>()
         snapshot.appendSections([0])
         snapshot.appendItems(seedWords)
-        dataSource?.apply(delayedSnapshot: snapshot, animatingDifferences: true) { [weak self] in
+        dataSource?.apply(delayedSnapshot: snapshot, animatingDifferences: false) { [weak self] in
             guard let self = self else { return }
             self.collectionView.scrollToBottom(animated: true)
             self.heightConstraint?.constant = self.collectionView.contentSize.height
