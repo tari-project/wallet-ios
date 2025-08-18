2cdb7adffb643f05873117c72b2da44ce3affe94
thakel <tomas@tari.com>
2025-08-18 14:17:00 +0200

    Required update sheet

---
+++ MobileWallet/Screens/UpdateRequired/UpdateRequired.swift
@@ -0,0 +1,114 @@
+//  UpdateRequired.swift
+	
+/*
+	Package MobileWallet
+	Created by Tomas Hakel on 18.08.2025
+	Using Swift 6.0
+	Running on macOS 15.5
+
+	Copyright 2019 The Tari Project
+
+	Redistribution and use in source and binary forms, with or
+	without modification, are permitted provided that the
+	following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice,
+	this list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above
+	copyright notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+
+	3. Neither the name of the copyright holder nor the names of
+	its contributors may be used to endorse or promote products
+	derived from this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+	CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+	OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+	NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+	HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+	OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+import SwiftUI
+
+struct UpdateRequired: View {
+    private let title: String
+    private let message: String
+    private let ctaTitle: String
+    private let isDismissable: Bool
+    private let update: () -> Void
+    private let dismiss: () -> Void
+    
+    init(
+        title: String,
+        message: String,
+        ctaTitle: String,
+        isDismissable: Bool,
+        update: @escaping () -> Void,
+        dismiss: @escaping () -> Void
+    ) {
+        self.title = title
+        self.message = message
+        self.ctaTitle = ctaTitle
+        self.isDismissable = isDismissable
+        self.update = update
+        self.dismiss = dismiss
+    }
+    
+    var body: some View {
+        VStack(spacing: 32) {
+            text
+            actions
+        }
+        .padding(24)
+        .interactiveDismissDisabled(!isDismissable)
+    }
+}
+
+@MainActor
+private extension UpdateRequired {
+    var text: some View {
+        VStack(spacing: 8) {
+            Text(title)
+                .modalTitle()
+                .foregroundColor(.primaryText)
+            Text(message)
+                .body()
+                .foregroundColor(.secondaryText)
+                .multilineTextAlignment(.center)
+        }
+    }
+    
+    var actions: some View {
+        VStack(spacing: 8) {
+            TariButton(ctaTitle, style: .primary, size: .large) {
+                update()
+            }
+            if isDismissable {
+                TariButton("Cancel", style: .text, size: .medium) {
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    UpdateRequired(
+        title: "Update required",
+        message: "Please update",
+        ctaTitle: "Update",
+        isDismissable: true,
+        update: { },
+        dismiss: { }
+    )
+}
